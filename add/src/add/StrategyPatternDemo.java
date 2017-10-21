b831f902d36aeab58775ef538f5e16606df60308
Muhammad <Muhammad@192.168.10.20>
2017-10-21 15:01:39 +0500

    Add

---
+++ add/src/add/StrategyPatternDemo.java
@@ -0,0 +1,8 @@
+package add;
+
+public class StrategyPatternDemo {
+	public static void main(String[] args) {
+	      Context context = new Context(new OperationAdd());		
+	      System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
+	}
+}
